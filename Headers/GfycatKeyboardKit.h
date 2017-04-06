ad4a9b04ba5da2337d66f8af146411dde6541b3a
Victor Pavlychko <victor.pavlychko@gmail.com>
2017-04-06 17:08:08 +0300

    Pod

---
+++ Headers/GfycatKeyboardKit.h
@@ -0,0 +1,73 @@
+//
+//  GfycatKeyboardKit.h
+//  GfycatKeyboardKit
+//
+//  Created by Oleksii Chubenko on 4/3/17.
+//  Copyright © 2017 Gfycat. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for GfycatKeyboardKit.
+FOUNDATION_EXPORT double GfycatKeyboardKitVersionNumber;
+
+//! Project version string for GfycatKeyboardKit.
+FOUNDATION_EXPORT const unsigned char GfycatKeyboardKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <GfycatKeyboardKit/PublicHeader.h>
+
+// Utils.Common
+#import <GfycatKeyboardKit/GFYArrayDiff.h>
+// Utils.UIKit
+#import <GfycatKeyboardKit/UICollectionView+GfycatInterface.h>
+#import <GfycatKeyboardKit/UIColor+GfycatInterface.h>
+#import <GfycatKeyboardKit/UINib+GfycatInterface.h>
+#import <GfycatKeyboardKit/UIScrollView+GfycatInterface.h>
+#import <GfycatKeyboardKit/UITableView+GfycatInterface.h>
+// Utils.UICollectionView
+#import <GfycatKeyboardKit/GFYPresenterView.h>
+#import <GfycatKeyboardKit/GFYReusableView.h>
+#import <GfycatKeyboardKit/GFYCollectionViewCell.h>
+#import <GfycatKeyboardKit/GFYCollectionViewManager.h>
+// Utils.ViewControllers
+#import <GfycatKeyboardKit/GFYSimpleContainerViewController.h>
+// Utils.Views
+#import <GfycatKeyboardKit/GFYActivityGradientBar.h>
+#import <GfycatKeyboardKit/GFYActivityBarIndicator.h>
+#import <GfycatKeyboardKit/GFYBackgroundView.h>
+// Providers.Cache
+#import <GfycatKeyboardKit/GfycatApiCache.h>
+#import <GfycatKeyboardKit/GfycatBlackListCache.h>
+#import <GfycatKeyboardKit/GfycatCachedApi.h>
+#import <GfycatKeyboardKit/GfycatInterfaceCache.h>
+#import <GfycatKeyboardKit/GfycatSimpleCache.h>
+// Providers.Source
+#import <GfycatKeyboardKit/GFYArraySource.h>
+#import <GfycatKeyboardKit/GFYCategoryMediaArraySource.h>
+#import <GfycatKeyboardKit/GFYRecentMediaArraySource.h>
+#import <GfycatKeyboardKit/GFYSearchMediaArraySource.h>
+// Categories
+#import <GfycatKeyboardKit/GfycatMedia+GFYArrangable.h>
+#import <GfycatKeyboardKit/GfycatCategory+GFYArrangable.h>
+// Components.Browser
+#import <GfycatKeyboardKit/GFYBrowserSettings.h>
+#import <GfycatKeyboardKit/GFYBrowserController.h>
+#import <GfycatKeyboardKit/GFYBrowserViewController.h>
+// Components.CategoryPicker
+#import <GfycatKeyboardKit/GFYCategoryPickerSettings.h>
+#import <GfycatKeyboardKit/GFYCategoryPickerViewController.h>
+#import <GfycatKeyboardKit/GFYAnimatedCategoryCollectionViewCell.h>
+#import <GfycatKeyboardKit/GFYRecentCategoryCollectionViewCell.h>
+// Components.MediaPicker
+#import <GfycatKeyboardKit/GFYMediaPickerSettings.h>
+#import <GfycatKeyboardKit/GFYMediaPickerViewController.h>
+#import <GfycatKeyboardKit/GFYAnimatedMediaCollectionViewCell.h>
+// Components.MediaView
+#import <GfycatKeyboardKit/GFYMediaView.h>
+// Components.SearchHistory
+#import <GfycatKeyboardKit/GFYSearchHistoryViewController.h>
+// Components.Reporting
+#import <GfycatKeyboardKit/GFYFlagMediaViewControllerSettings.h>
+#import <GfycatKeyboardKit/GFYFlagMediaViewController.h>
+// Analytics
+#import <GfycatKeyboardKit/GFYInterfaceAnalytics.h>
